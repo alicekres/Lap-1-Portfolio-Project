d123d12fd3cea0d67c693f5dfc1db6dc17117852
William <williamsessions23@gmail.com>
2022-05-18 13:17:57 +0100

    added patch functions and better data

--- controllers/attractions.js
+++ controllers/attractions.js
@@ -30,6 +30,20 @@ router.post("/", (req, res) => {
   res.status(201).send(newAttraction);
 });
 
+router.patch("/:id", (req, res) => {
+  if (req.body.reactions) {
+    const attractionsId = req.params.id - 1;
+    let newEmojiCount = req.body.reactions - 1;
+    attractionsData[attractionsId].reactions[newEmojiCount]++;
+    res.send(attractionsData[attractionsId]);
+  } else if (req.body.reply) {
+    const attractionsId = req.params.id - 1;
+    let newReply = req.body.reply;
+    attractionsData[attractionsId].replies.unshift(newReply);
+    res.send(newReply);
+  }
+});
+
 router.delete("/:id", (req, res) => {
   /* const attractionsId = parseInt(req.params.id);
   const attractionsToDestroy = attractions.findById(attractionsId);
